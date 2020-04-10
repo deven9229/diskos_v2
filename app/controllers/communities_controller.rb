8ab7f4da153fe8576ccda372b6e150bf7e9c4a05
Deven Gurung <deven9229@gmail.com>
2020-04-10 22:53:01 +0600

    add summary to column to communities

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -8,6 +8,7 @@ class CommunitiesController < ApplicationController
 
   def show
     @posts = @community.posts
+    @subscriber_count = @community.subscribers.count
   end
 
   def new
@@ -33,7 +34,7 @@ class CommunitiesController < ApplicationController
   end
 
   def community_values
-    params.require(:community).permit(:name, :url, :rules)
+    params.require(:community).permit(:name, :url, :rules, :summary)
   end
 
 end
---
+++ db/migrate/20200410165048_add_summary_to_communities.rb
@@ -0,0 +1,6 @@
+class AddSummaryToCommunities < ActiveRecord::Migration[6.0]
+  def change
+    add_column :communities, :summary, :string
+  end
+end
+ 
