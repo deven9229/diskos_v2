3d1f491cd1b6a7b15a94bcde7e2a3a278940ec41
Deven Gurung <deven9229@gmail.com>
2020-04-11 14:46:26 +0600

    created controller for comments and implementated the feature

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,6 +7,7 @@ class Account < ApplicationRecord
 	has_many :subscriptions
 	has_many :communities, through: :subscriptions
 	has_many :posts
+	has_many :comments
 
 	validates_presence_of :first_name, :last_name, :username
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :posts
   end
   resources :subscriptions
+  resources :comments, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root to: "public#index"
 end
