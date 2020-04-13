859556c928706148a16b96c29ce6c78d027b68f7
Deven Gurung <deven9229@gmail.com>
2020-04-13 21:48:53 +0600

    added jquery and created includes folder with vote.js file

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ApplicationRecord
     belongs_to :community
     has_many :comments
     validates_presence_of :title, :body, :account_id, :community_id
+
+    def score
+    # difference between upvotes and downvotes
+        0
+    end
 end
